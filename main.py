951df7060ef574e73bba7a25c484140995a37fde
Etherion Flame <claybarr2@gmail.com>
2023-11-14 13:17:43 -0600

    v0.6 menu system add mongo db fully integrated with parsing for txt,json,and csv files in a specific format, not much error handling

--- main.py
+++ main.py
@@ -1,131 +1,293 @@
-# This is a sample Python script.
+from abc import ABC, abstractmethod
+from typing import List
+import json
+import csv
+from pip._internal.resolution.resolvelib.base import Candidate
+from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from typing import Optional
+# FileParsingStrategy interface
+class FileParsingStrategy(ABC):
+    @abstractmethod
+    def parse_file(self, file_path: str) -> List[Candidate]:
+        pass
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import pymongo
-username = "your_username"
-password = "your_password"
-cluster_name = "your_cluster"
-database_name = "your_database"
-connection_string = f"mongodb+srv://{username}:{password}@{cluster_name}.mongodb.net/{database_name}?retryWrites=true&w=majority"
+class FileParsingPolicy:
+    def determine_strategy(self, file_path: str) -> Optional[FileParsingStrategy]:
+        file_extension = file_path.split('.')[-1].lower()
+
+        if file_extension == 'csv':
+            return CSVFileParser()
+        elif file_extension == 'json':
+            return JSONFileParser()
+        elif file_extension == 'txt':
+            return TextFileParser()
+        else:
+            print(f"Unsupported file type: {file_extension}")
+            return None
+
+# Concrete strategy for CSV files
+class CSVFileParser(FileParsingStrategy):
+    def parse_file(self, file_path: str) -> List[Candidate]:
+        candidates = []
+        try:
+            with open(file_path, 'r') as file:
+                csv_reader = csv.reader(file)
+                # Assuming the CSV file has a header row
+                header = next(csv_reader)
+                for row in csv_reader:
+                    candidate = Candidate(
+                        first_name=row[0],
+                        last_name=row[1],
+                        DOB=row[2],
+                        party=row[3],
+                        SOC=row[4],
+                        position=row[5]
+                    )
+                    candidates.append(candidate)
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+        return candidates
+
+#Concrete strategy for JSON files
+class JSONFileParser(FileParsingStrategy):
+    def parse_file(self, file_path: str) -> List[Candidate]:
+        candidates = []
+        try:
+            with open(file_path, 'r') as file:
+                json_data = file.read()
+                # Assuming the JSON file contains an array of candidates
+                candidates_data = json.loads(json_data)
+                for candidate_data in candidates_data:
+                    candidate = Candidate(
+                        first_name=candidate_data.get('first_name', ''),
+                        last_name=candidate_data.get('last_name', ''),
+                        DOB=candidate_data.get('DOB', ''),
+                        party=candidate_data.get('party', ''),
+                        SOC=candidate_data.get('SOC', ''),
+                        position=candidate_data.get('position', '')
+                    )
+                    candidates.append(candidate)
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON in file: {file_path}")
+        return candidates
+# concrete strategy for txt files
+# New concrete strategy for text files
+class TextFileParser(FileParsingStrategy):
+    def parse_file(self, file_path: str) -> List[Candidate]:
+        candidates = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                data = line.strip().split(',')
+                if len(data) == 6:
+                    candidate = Candidate(
+                        first_name=data[0],
+                        last_name=data[1],
+                        DOB=data[2],
+                        party=data[3],
+                        SOC=data[4],
+                        position=data[5]
+                    )
+                    candidates.append(candidate)
+                else:
+                    print(f"Invalid data format in line: {line}")
+
+        return candidates
+
+
+# Concrete strategy for MongoDB writing
+class MongoDBWriter(FileParsingStrategy):
+
+    def parse_file(self, file_path: str) -> List[Candidate]:
+        #NOT CURRENTLY IMPLEMENTED OR PLANNED TO BE IMPLEMENTED
+        candidates = self._read_file(file_path)
+        ###return candidates
+
+    def write_candidates_to_mongodb(self, candidates: List[Candidate]):
+        # This method is specifically for writing candidates to MongoDB without reading from a file
+        self._write_to_mongodb(candidates)
+
+    def _read_file(self, file_path: str) -> List[Candidate]:
+        # Use the specified parser to read data from the file
+        return self.parser.parse_file(file_path)
+
+    def _write_to_mongodb(self, candidates: List[Candidate]):
+        # Implement MongoDB writing logic here
+        print("Sending Data to Database:")
+        uri = "mongodb+srv://ClayBarr:GenericPassword@candidateregistration.yhgqzoi.mongodb.net/?retryWrites=true&w=majority"
+        # Create a new client and connect to the server
+        client = MongoClient(uri, server_api=ServerApi('1'))
+        db = client['candidates_db']
+        collection = db['candidates']
+        for candidate in candidates:
+            data = {
+                "first_name": candidate.getFirstName(),
+                "last_name": candidate.getLastName(),
+                "DOB": candidate.getDOB(),
+                "party": candidate.getParty(),
+                "SOC": candidate.getSOC(),
+                "position": candidate.getPosition()
+            }
+            result = collection.insert_one(data)
+            # Retrieve the generated MongoDB _id and store it in cand_ID
+            print(result.inserted_id," Remember this as the your id")
+        client.close()
+
+# Context class (Candidate) using the strategy
 class Candidate:
-    def __init__(self, first_name, last_name, DOB, cand_ID, party, SOC, position):
+    def __init__(self, first_name, last_name, DOB, party, SOC, position):
         self.first_name = first_name
         self.last_name = last_name
         self.DOB = DOB
-        self.cand_ID = cand_ID
+
         self.party = party
         self.SOC = SOC
         self.position = position
+        self.parser = None  # Added attribute to store the current strategy
 
-        def getFirstName(self):
-            return self.first_name
+    def getFirstName(self):
+        return self.first_name
 
-        def getLastName(self):
-            return self.last_name
+    def getLastName(self):
+        return self.last_name
 
-        def getDOB(self):
-            return self.DOB
+    def getDOB(self):
+        return self.DOB
 
-        def getParty(self):
-            return self.party
+    def getParty(self):
+        return self.party
 
-        def getSOC(self):
-            return self.SOC
+    def getSOC(self):
+        return self.SOC
 
-        def getPosition(self):
-            return self.position
+    def getPosition(self):
+        return self.position
 
-def registerCandidate():
+    def set_parser(self, parser: FileParsingStrategy):
+        self.parser = parser  # Set the current strategy
 
-    newFirstName = input("Enter your first name: ")
-    newLastName = input("Enter your last name: ")
-    newDOB = input("Enter your Date of Birth: ")
-    newPolParty = input("Enter your political party: ")
-    newSOC = input("Enter your Social Security: ")
-
-    newCandidate = Candidate(newFirstName,newLastName,newDOB,newPolParty,newSOC)
-    return newCandidate
-
-def updateCandidate():
-    print("Under Construction")
-    return
-
-def printCandidates(candidateList):
-    for x in range(0,len(candidateList)):
-        print("Candidate: " + str(x + 1) + "\n " +
-              "------------------------")
-        print("Name: " + candidateList[x].getFirstName() + " " + candidateList[x].getLastName())
-        print("DOB: " + candidateList[x].getDOB())
-        print("Political Party: " + candidateList[x].getParty())
-        print("SOC: " + candidateList[x].getSOC() + "\n ")
-        print("Office: " + candidateList[x].getPOS() + "\n")
-
+    def parse_file(self, file_path: str) -> List[Candidate]:
+        if self.parser is not None:
+            return self.parser.parse_file(file_path)
+        else:
+            print("Error: No parser set. Please use set_parser to set a parsing strategy.")
+            return []
 
 def main():
     global inMenu
     inMenu = True
-    # a dictionary to hold all valid candidates
-    candidateList = []
-    while(inMenu):
+    while (inMenu):
         print("hello welcome to the hotpatchers™ candidate registration system (>^—^)>♡ \n" +
               "Please select one of the following options \n" +
               "1. Register \n" +
-              "2. Update information\n" +
+              "2. View\\Update information\n" +
               "3. Print candidate list \n" +
               "4. Exit program")
         print("-----------------------")
-
         userChoice = input("Menu Decision Num: ")
         choiceNum = int(userChoice)
 
         if choiceNum == 1:
-            candidateList.append(registerCandidate())
+            file_name = input("Please input the exact file location for the your candidate form")
+            assert os.path.isfile(file_name)
+            csv_parser = CSVFileParser()
+            json_parser = JSONFileParser()
+            mongodb_writer = MongoDBWriter()
+            txt_parser = TextFileParser()
+            candidate = Candidate(first_name="John", last_name="Doe", DOB="1990-01-01", party="Independent",
+                                  SOC="123-45-6789", position="Senator")
+
+            # Create an instance of the policy class
+            parsing_policy = FileParsingPolicy()
+            # Determine the strategy based on the file type
+            strategy = parsing_policy.determine_strategy(file_name)
+
+            if strategy:
+                # Set the determined strategy for the candidate
+                candidate.set_parser(strategy)
+
+                # Parse the file using the determined strategy
+                file_data = candidate.parse_file(file_name)
+                candidate.set_parser(mongodb_writer)
+                # Print the parsed data
+                for candidate_data in file_data:
+                    print(f"Parsed Data ({file_name}):", candidate_data.getFirstName(), candidate_data.getLastName(),
+                          candidate_data.getDOB(), candidate_data.getParty(), candidate_data.getSOC(),
+                          candidate_data.getPosition())
+
+                mongodb_writer.write_candidates_to_mongodb(file_data)
+
             pause = input("Candidate registered, press enter to continue:")
         elif choiceNum == 2:
-            updateCandidate()
+            # not implemented
             pause = input("Candidate updated, press enter to continue:")
         elif choiceNum == 3:
-            printCandidates(candidateList)
+            # not implemented
             pause = input("list printed, press enter to continue:")
         elif choiceNum == 4:
             inMenu = False
         else:
             pause = input("invalid input, press enter to continue:")
+# Example usage
+if __name__ == "__main__":
+    main()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    #main()
-    username = "your_username"#Replace all with credentials for MongDb
-    password = "your_password"
-    cluster_name = "your_cluster"
-    database_name = "your_database"
-    connection_string = f"mongodb+srv://{username}:{password}@{cluster_name}.mongodb.net/{database_name}?retryWrites=true&w=majority"
+    # Create instances of parsing strategies
+    #file_name="C:\\Users\\roboc\\PycharmProjects\\CandidateRegistrationgFileParsers\\jsontest1.json"
+    #file_name="C:\\Users\\roboc\PycharmProjects\\CandidateRegistrationgFileParsers\\txttestfile1.txt"
+    '''assert os.path.isfile(file_name)
+    csv_parser = CSVFileParser()
+    json_parser = JSONFileParser()
+    mongodb_writer = MongoDBWriter()
+    txt_parser = TextFileParser()
 
-    # Initialize the MongoDB client and connect to a MongoDB server
-    client = pymongo.MongoClient(connection_string)
+    # Create an instance of the context class (Candidate)
+    candidate = Candidate(first_name="John", last_name="Doe", DOB="1990-01-01",  party="Independent", SOC="123-45-6789", position="Senator")
 
-    # Create or use an existing database
-    db = client["candidates_database"]
-    collection = db["candidates_collection"]
+    # Create an instance of the policy class
+    parsing_policy = FileParsingPolicy()
+    # Determine the strategy based on the file type
+    strategy = parsing_policy.determine_strategy(file_name)
 
-    candidate = Candidate("Jon","Snow",1/23/23,0,"Democrat","425-11-2222","Dog Catcher")
+    if strategy:
+        # Set the determined strategy for the candidate
+        candidate.set_parser(strategy)
 
-    candidate_data = {
-        "first_name": candidate.first_name,
-        "last_name": candidate.last_name,
-        "DOB": candidate.DOB,
-        "cand_ID": candidate.cand_ID,
-        "party": candidate.party,
-        "SOC": candidate.SOC,
-        "position": candidate.position
-    }
-    result = collection.insert_one(candidate_data)
+        # Parse the file using the determined strategy
+        file_data = candidate.parse_file(file_name)
+        candidate.set_parser(mongodb_writer)
+        # Print the parsed data
+        for candidate_data in file_data:
+            print(f"Parsed Data ({file_name}):", candidate_data.getFirstName(), candidate_data.getLastName(),
+                  candidate_data.getDOB(), candidate_data.getParty(), candidate_data.getSOC(),
+                  candidate_data.getPosition())
+            mongodb_writer.write_candidates_to_mongodb(file_data)
 
-    print(candidate.first_name)
-    if result.acknowledged:
-        print("Candidate data inserted with ID:", result.inserted_id)
 
-    client.close()
+    # Parse a CSV file using the current strategy
+    #csv_data = csv_parser.parse_file(file_name)
+    #print("CSV Data:", csv_data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Switch to the JSON parsing strategy
+   # candidate.set_parser(json_parser)
+
+    # Parse a JSON file using the new strategy
+    #json_data = json_parser.parse_file(file_name)
+   # print("JSON Data:", json_data)
+
+    #candidate.set_parser(txt_parser)
+
+    #txt_data = candidate.parse_file(file_name)
+    #print("Text File Data:", txt_data)
+
+    # Switch to the MongoDB writing strategy
+    #candidate.set_parser(mongodb_writer)
+
+    # Parse a file and write to MongoDB using the new strategy
+    #mongodb_writer.write_candidates_to_mongodb(txt_data)
+    #mongodb_writer.write_candidates_to_mongodb(json_data)
+    #mongodb_writer.write_candidates_to_mongodb(csv_data)'''
